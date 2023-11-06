aecf0ae8eb5dc57ab0d94a9d338d6ab2faf175eb
elioenaiferrari <elioenaiferrari@gmail.com>
2023-11-06 11:30:00 -0300

    add docs

--- cmd/init.go
+++ cmd/init.go
@@ -34,14 +34,10 @@ func (c *Config) GetEnv(label string) Env {
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
-	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:     "init",
+	Short:   "Initialize forgeSQL",
+	Long:    "Initialize forgeSQL creating a .forgesql.yml file in the current directory.",
+	Example: "forgesql init",
 	Run: func(cmd *cobra.Command, args []string) {
 		var migrationsPath string
 		var envsSeparetedByComma string
